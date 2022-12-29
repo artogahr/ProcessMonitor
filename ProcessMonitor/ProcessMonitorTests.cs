68b73d764d815e4a5466b3424b5952cc8a7fa53a
artogahr <artogahr@gmail.com>
2022-12-29 12:03:53 +0100

    Adding finished project

---
+++ ProcessMonitor/ProcessMonitorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessMonitor.Tests
+{
+    public class ProcessMonitorTests
+    {
+        [Test]
+        public void TestProcessKilling()
+        {
+            // In real life we could use some dummy binary for it, but I'm using notepad for now
+            ProcessStartInfo startInfo = new ProcessStartInfo("notepad.exe");
+            
+            Process testProcess = Process.Start(startInfo);
+
+            Process monitorProcess = Process.Start("ProcessMonitor.exe", "notepad 1 1");
+
+            // Sleep for 2 minutes to allow the process to be killed
+            Thread.Sleep(2 * 60 * 1000);
+
+            // Check if the test process is still running
+            Process[] processes = Process.GetProcessesByName("testProcess");
+            Assert.IsEmpty(processes, "Test process was not killed by the monitoring utility.");
+
+            monitorProcess.Kill();
+            testProcess.Kill();
+        }
+    }
+
+}
---
+++ ProcessMonitor/Program.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+namespace ProcessMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Error: Insufficient arguments provided. Please provide the process name, maximum lifetime and monitoring frequency.");
+                Console.WriteLine("Usage: ProcessMonitor.exe process_name maximum_lifetime (in minutes) monitoring_frequency (also in minutes)");
+                return;
+            }
+
+            string processName = args[0];
+            int maximumLifetime;
+            int monitoringFrequency;
+
+            if (!int.TryParse(args[1], out maximumLifetime))
+            {
+                Console.WriteLine("Error: Invalid maximum lifetime provided. Please provide an integer value.");
+                return;
+            }
+
+            if (!int.TryParse(args[2], out monitoringFrequency))
+            {
+                Console.WriteLine("Error: Invalid monitoring frequency provided. Please provide an integer value for the mniutes.");
+                return;
+            }
+
+            Console.WriteLine($"Monitoring process '{processName}' with maximum lifetime of {maximumLifetime} minutes and monitoring frequency of {monitoringFrequency} minutes.");
+            Console.WriteLine("Press 'q' to stop the monitoring.");
+
+            while (true)
+            {
+                if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+
+                Process[] processes = Process.GetProcessesByName(processName);
+
+                // Iterate over the processes and check if any of them have exceeded the maximum lifetime
+                foreach (Process process in processes)
+                {
+                    if (process.StartTime.AddMinutes(maximumLifetime) < DateTime.Now)
+                    {
+                        Console.WriteLine($"Killing process '{process.ProcessName}' with PID {process.Id} as it has exceeded the maximum lifetime of {maximumLifetime} minutes.");
+                        process.Kill();
+                    }
+                }
+
+                Thread.Sleep(monitoringFrequency * 60 * 1000);
+            }
+        }
+    }
+
+}
